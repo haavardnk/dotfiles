5aed904c05b282a05c55ce2e1fc9fdd83e8255e5
Håvard Nordhus Kråkenes <haavardnk@outlook.com>
2024-10-28 22:30:17 +0100

    Add .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 19
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.scrollback_lines = 3000
+config.color_scheme = "Tokyo Night"
+
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+config.enable_tab_bar = false
+
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = false
+
+return config
