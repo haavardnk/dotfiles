9e52fc0c8085c7da15d09cd9446a4c8836a00581
Håvard Nordhus Kråkenes <haavardnk@outlook.com>
2024-12-19 14:00:52 +0100

    Update dotfiles

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,17 +1,24 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- Set the font and font size
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 19
+
+-- Window appearance and behavior settings
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.color_scheme = "Catppuccin Frappe"
 
+-- Tab bar settings
 config.use_fancy_tab_bar = false
-config.status_update_interval = 1000
 config.enable_tab_bar = false
 
+-- Status update interval in milliseconds
+config.status_update_interval = 1000
+
+-- Key composition settings for Alt keys
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = false
 
